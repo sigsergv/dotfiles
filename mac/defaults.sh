6449a12b7cfd17ff625bfad0d5578c40e2d27421
Sergey Stolyarov <sergei@regolit.com>
2014-10-20 10:17:45 +0700

    Updated mac config

--- mac/defaults.sh
+++ mac/defaults.sh
@@ -21,6 +21,27 @@ defaults write com.apple.screencapture disable-shadow -bool true
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
 defaults write com.apple.Finder FXPreferredViewStyle -string "Nlsv"
 
+# Use current directory as default search scope in Finder
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+
+# Show Path bar in Finder
+defaults write com.apple.finder ShowPathbar -bool true
+
+# Show Status bar in Finder
+defaults write com.apple.finder ShowStatusBar -bool true
+
+# Set a blazingly fast keyboard repeat rate
+#defaults write NSGlobalDomain KeyRepeat -int 0.02
+
+# Set a shorter Delay until key repeat
+#defaults write NSGlobalDomain InitialKeyRepeat -int 12
+
+# Enable Safari’s debug menu
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+
+# Add a context menu item for showing the Web Inspector in web views
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
 # Enable highlight hover effect for the grid view of a stack (Dock)
 defaults write com.apple.dock mouse-over-hilte-stack -bool true
 
