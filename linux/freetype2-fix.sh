0012bb9ae8816667a9e49e6fe44ad02766d746d3
Sergey Stolyarov <sergei@regolit.com>
2017-11-30 12:54:42 +0700

    Added new script

---
+++ linux/freetype2-fix.sh
@@ -0,0 +1,18 @@
+# Subpixel hinting mode can be chosen by setting the right TrueType interpreter
+# version. The available settings are:
+#
+#     truetype:interpreter-version=35  # Classic mode (default in 2.6)
+#     truetype:interpreter-version=38  # Infinality mode
+#     truetype:interpreter-version=40  # Minimal mode (default in 2.7)
+#
+# There are more properties that can be set, separated by whitespace. Please
+# refer to the FreeType documentation for details.
+
+# copy this file:
+# To global environment:
+#     sudo cp freetype2-fix.sh /etc/profile.d/freetype2.sh
+# or to kde profile
+#     cp freetype2-fix.sh $HOME/.config/plasma-workspace/env/freetype.sh
+
+# Uncomment and configure below
+export FREETYPE_PROPERTIES="truetype:interpreter-version=35"
