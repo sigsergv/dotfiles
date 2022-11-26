1af49088ea8f5611dd8d0657f58451e969207dd9
Sergey Stolyarov <sergei@regolit.com>
2022-11-26 15:19:06 +0700

    New script

---
+++ sublimetext/install-mac.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+CWD=$(pwd)
+TWD="${HOME}/Library/Application Support/Sublime Text 3/Packages"
+
+set -x
+rm -f "${TWD}/SIGSERGV.tmTheme"
+ln -s "${CWD}/SIGSERGV.tmTheme" "${TWD}"
+
+rm -f "${TWD}/Theme - SIGSERGV"
+ln -s "${CWD}/Theme - SIGSERGV" "${TWD}"
