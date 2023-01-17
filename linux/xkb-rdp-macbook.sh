fe3801558fb0492b1642f12602b76eb5389f9f2f
Sergey Stolyarov <sergei@regolit.com>
2023-01-17 11:45:38 +0700

    Updated script

--- linux/xkb-rdp-macbook.sh
+++ linux/xkb-rdp-macbook.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-#setxkbmap -layout 'us,ru' -variant ',winkeys' -option 'grp:alt_shift_toggle,grp_led:scroll,apple:badmap'
+setxkbmap -layout 'us,ru' -variant ',winkeys' -option 'grp:alt_shift_toggle,grp_led:scroll,apple:badmap'
 xmodmap -e 'keycode 94 = grave asciitilde Cyrillic_io Cyrillic_IO'
