2ade42d9e74599eda2b02e886def17f358fe9706
Sergey Stolyarov <sergei@regolit.com>
2024-08-10 22:04:05 +0700

    Added source for keyboard layout flags

---
+++ mac/Ilya Birman Typography Layout/flags/README.md
@@ -0,0 +1,5 @@
+This directory contains source files for icons.
+
+To build use command like this:
+
+    make classic-set
---
+++ mac/Ilya Birman Typography Layout/flags/build.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -x
+
+SRC=$1
+SRC_DIR=$(readlink -f $SRC)
+START_DIR=$(pwd)
+
+if [ -z "$SRC_DIR" ]; then
+	exit 1
+fi
+
+TMDIR=$(mktemp -d)
+# TMPDIR=tmp
+
+cd $TMPDIR
+mkdir 'English - Ilya Birman Typography.iconset' 'Russian - Ilya Birman Typography.iconset'
+cp "${SRC_DIR}/en/"*.png 'English - Ilya Birman Typography.iconset'
+cp "${SRC_DIR}/ru/"*.png 'Russian - Ilya Birman Typography.iconset'
+iconutil --convert icns 'English - Ilya Birman Typography.iconset'
+iconutil --convert icns 'Russian - Ilya Birman Typography.iconset'
+rm -rf 'English - Ilya Birman Typography.iconset' 'Russian - Ilya Birman Typography.iconset'
+mkdir -p 'Library/Keyboard Layouts/Ilya Birman Typography Layout.bundle/Contents/Resources'
+cp 'English - Ilya Birman Typography.icns' 'Russian - Ilya Birman Typography.icns' 'Library/Keyboard Layouts/Ilya Birman Typography Layout.bundle/Contents/Resources'
+zip_file="${START_DIR}/Ilya Birman Typography Layout flags - ${SRC}.zip"
+rm "${zip_file}"
+zip -r "${zip_file}" 'Library'
+rm -r $TMDIR
