d20fe9de9eb8dd6cc100a914d1c0055536695196
Sergey Stolyarov <sergei@regolit.com>
2024-09-16 09:26:21 +0300

    Updated configs

--- devices/huawei-matebook-16s/xinput-persist.sh
+++ devices/huawei-matebook-16s/xinput-persist.sh
@@ -2,5 +2,8 @@
 #
 # Add to autostart. For KDE, for example, use Autostart configuration widget
 
+# touchpad properties
 xinput set-prop 9 'libinput Scrolling Pixel Distance' 25
+
+# disable touchscreen
 xunput disable 11
