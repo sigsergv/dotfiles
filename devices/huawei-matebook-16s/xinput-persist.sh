24114c89275000e11fe3a21b9c08a638eb4573d5
Sergey Stolyarov <sergei@regolit.com>
2024-09-16 13:04:09 +0300

    Fixed huawei scripts
    Detect actual device IDs dynamically.

--- devices/huawei-matebook-16s/xinput-persist.sh
+++ devices/huawei-matebook-16s/xinput-persist.sh
@@ -3,7 +3,9 @@
 # Add to autostart. For KDE, for example, use Autostart configuration widget
 
 # touchpad properties
-xinput set-prop 9 'libinput Scrolling Pixel Distance' 25
+touchpad_id=$(xinput list | grep Touchpad | grep -o 'id=[0-9]\+' | grep -o '[0-9]\+')
+xinput set-prop $touchpad_id 'libinput Scrolling Pixel Distance' 25
 
 # disable touchscreen
-xunput disable 11
+touchscreen_id=$(xinput | grep GXTP738X | grep -o 'id=[0-9]\+' | grep -o '[0-9]\+')
+xinput disable $touchscreen_id
