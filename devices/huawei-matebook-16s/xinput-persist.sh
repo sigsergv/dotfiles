1311d73eb46b16783dba32c1880de5dd0f634114
Sergey Stolyarov <sergei@regolit.com>
2025-03-22 10:11:18 +0700

    Updated scripts for huawei

--- devices/huawei-matebook-16s/xinput-persist.sh
+++ devices/huawei-matebook-16s/xinput-persist.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 #
 # Add to autostart. For KDE, for example, use Autostart configuration widget
+#
+# This script tunes input devices:
+#   * disables touchscreen;
+#   * set more comfortable scrolling behavior for touchpad
+
+probe_device_ids=$(xinput list | grep GXTP7863:00 | grep -o 'id=[0-9]\+' | grep -o '[0-9]\+')
+for d in $probe_device_ids; do
+	xinput list-props $d | grep -q Tapping
+	if [ $? -eq 0 ]; then
+		touchpad_id=$d
+		break
+	fi
+done
 
 # touchpad properties
-touchpad_id=$(xinput list | grep Touchpad | grep -o 'id=[0-9]\+' | grep -o '[0-9]\+')
-xinput set-prop $touchpad_id 'libinput Scrolling Pixel Distance' 25
+if [ ! -z $touchpad_id ]; then
+	xinput set-prop $touchpad_id 'libinput Scrolling Pixel Distance' 25
+fi
 
 # disable touchscreen
 touchscreen_id=$(xinput | grep GXTP738X | grep -o 'id=[0-9]\+' | grep -o '[0-9]\+')
